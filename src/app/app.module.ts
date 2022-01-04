9351cc98841925c0ef72a81f0df8e354de0a6ab9
yoabhi98 <yoabhi98@gmail.com>
2022-01-04 17:44:22 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { Routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about.component';
+import { EmployeesComponent } from './employees.component';
+import { EmployeeComponent } from './employee.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, routing],
+  declarations: [
+    AppComponent,
+    AppComponent,
+    AboutComponent,
+    EmployeeComponent,
+    EmployeesComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
